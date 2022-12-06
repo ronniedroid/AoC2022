4eca9c8c9c23a5085884c5ef6c4f7e2e7860b173
ronniedroid <ronni.n.yousif@gmail.com>
2022-12-06 22:10:02 +0300

    solved day three

---
+++ src/ronniedroid/daythree.clj
@@ -0,0 +1,61 @@
+(ns ronniedroid.daythree
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input
+  (slurp (io/resource "daythreeinput.txt")))
+
+(def abc
+  (->
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    (str/split #"")
+    (->> (map #(keyword %)))
+    (interleave (range 1 53))
+    (->> (partition 2)
+         (map vec)
+         (into {}))))
+
+(def parsed-input
+  (-> input
+      (str/split #"\n")))
+
+(defn make-keys-get-val
+  [coll]
+  (->> coll
+       (map #(keyword %))
+       (map #(get abc %))))
+
+(defn split-and-compair
+  [coll]
+  (let [col (map #(vec (str/split % #"")) coll)
+        a (first col)
+        b (second col)
+        c (last col)]
+    (->> a
+         (filter (set b))
+         (filter (set c))
+         (distinct))))
+
+(defn part-one
+  [coll]
+  (->> coll
+       (map #(str/split % #""))
+       (map #(make-keys-get-val %))
+       (map #(let [len (/ (count %) 2)]
+               (split-at len %)))
+       (map #(let [a (first %)
+                   b (second %)]
+               (filter (set a) b)))
+       (map #(distinct %))
+       (flatten)
+       (apply +)))
+
+(defn part-two
+  [coll]
+  (->> coll
+       (partition 3)
+       (map #(split-and-compair %))
+       (flatten)
+       (make-keys-get-val)
+       (apply +)))
