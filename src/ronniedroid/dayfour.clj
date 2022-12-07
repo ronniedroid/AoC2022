23f96eaa46a658b7814bc096a847c24418b8fe7b
ronniedroid <ronni.n.yousif@gmail.com>
2022-12-07 12:32:33 +0300

    solved day four

---
+++ src/ronniedroid/dayfour.clj
@@ -0,0 +1,55 @@
+(ns ronniedroid.dayfour
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+(def input
+  (slurp (io/resource "dayfourinput.txt")))
+
+(defn get-pairs
+  [coll]
+  (->> coll
+       (mapv #(read-string %))
+       (partition 2)
+       (mapv #(range (first %) (+ 1 (last %))))
+       (partition 2)))
+
+(defn get-contains-or-overlaps
+  [coll c]
+  (let [a (sort (first coll))
+        b (sort (second coll))
+        a-length (count a)
+        b-length (count b)
+        smaller (if (< a-length b-length) a b)
+        results (->> (set/intersection (set a)
+                                       (set b)))]
+    (if (= c "contains")
+      (= smaller (sort results))
+      results)))
+
+(defn parse-input
+  [s]
+  (-> s
+      (str/replace #"\n|-|," " ")
+      str/trim
+      (str/split #" ")
+      get-pairs))
+
+(defn part-one
+  [coll]
+  (->> coll
+       parse-input
+       (map
+        #(get-contains-or-overlaps % "contains"))
+       (filter #(true? %))
+       count))
+
+(defn part-two
+  [coll]
+  (->> coll
+       parse-input
+       (map
+        #(get-contains-or-overlaps % "overlaps"))
+       (filter #(< 0 (count %)))
+       count))
