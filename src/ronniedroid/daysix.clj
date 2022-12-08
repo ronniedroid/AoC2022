1e1f42318aa1b94c0b37eea9afe91a8e46bef361
ronniedroid <ronni.n.yousif@gmail.com>
2022-12-08 18:18:23 +0300

    updated my day six solution

--- src/ronniedroid/daysix.clj
+++ src/ronniedroid/daysix.clj
@@ -4,25 +4,38 @@
 
 (def input (slurp "resources/daysixinput.txt"))
 
-(defn find-start-of
-  [coll subroutine]
-  (let [n (if (= subroutine "packet") 4 14)]
-    (->> coll
-         (partition n 1)
-         (map #(distinct %))
-         (filter #(= (count %) n))
-         first
-         str/join)))
+(defn find-marker
+  [string n]
+  (->> string
+       (str/trim-newline)
+       (partition n 1)
+       (map #(distinct %))
+       (map #(count %))
+       (take-while (complement #{n}))
+       count
+       (+ n)))
 
-(defn find-marker-of
-  [coll subroutine]
-  (let [n (if (= subroutine "packet") 4 14)]
-    (-> (str/replace-first
-         coll
-         (find-start-of coll subroutine)
-         "-")
-        (str/split #"")
-        (.indexOf "-")
-        (+ n)
-        (take input)
-        count)))
+;; first try
+
+;; (defn find-start-of
+;;   [coll subroutine]
+;;   (let [n (if (= subroutine "packet") 4 14)]
+;;     (->> coll
+;;          (partition n 1)
+;;          (map #(distinct %))
+;;          (filter #(= (count %) n))
+;;          first
+;;          str/join)))
+
+;; (defn find-marker-of
+;;   [coll subroutine]
+;;   (let [n (if (= subroutine "packet") 4 14)]
+;;     (-> (str/replace-first
+;;          coll
+;;          (find-start-of coll subroutine)
+;;          "-")
+;;         (str/split #"")
+;;         (.indexOf "-")
+;;         (+ n)
+;;         (take input)
+;;         count)))
